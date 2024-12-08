63ec4a73cc2fe53d696cd5277181c2b26264d9f3
Mehmood M.Ashraf <mehmoodfazlani@gmail.com>
2024-12-08 21:18:21 +0500

    Home Page Complete

---
+++ src/components/MenuCards.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image, { StaticImageData } from 'next/image'
+import MenuImage1 from "../../public/Images/Hero6Menu1.png"
+
+interface menuCardProps{
+    image: StaticImageData;
+    name: string;
+    detail: string;
+    price:  string
+}
+
+const MenuCards = (props: menuCardProps) => {
+const {image, name, detail, price} = props
+
+  return (
+    <div className='flex gap-1'>
+        <div>
+            <Image src={image} alt='' className='h-full'></Image>
+        </div>
+        <div className='font-inter flex flex-col gap-1 text-background'>
+            <p className='text-xl font-bold'>{name}</p>
+            <p className='lg:block hidden text-sm leading-[22px]'>{detail}</p>
+            <p className='text-primary text-lg leading-[26px] font-bold'>{price}</p>
+        </div>
+    </div>
+  )
+}
+
+export default MenuCards
